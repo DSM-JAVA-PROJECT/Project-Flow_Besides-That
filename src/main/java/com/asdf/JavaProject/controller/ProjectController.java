93b619f7c44298811806f75d2b1427f93e8b39e1
asdf <wnduf0405_@naver.com>
2021-09-05 11:08:56 +0900

    Provisional Notes

--- src/main/java/com/asdf/JavaProject/controller/ProjectController.java
+++ src/main/java/com/asdf/JavaProject/controller/ProjectController.java
@@ -1,7 +1,5 @@
 package com.asdf.JavaProject.controller;
 
-import com.asdf.JavaProject.dto.ProjectSchedule.CreateSchedule;
-import com.asdf.JavaProject.dto.ProjectSchedule.ModifySchedule;
 import com.asdf.JavaProject.dto.project.CreateProject;
 import com.asdf.JavaProject.dto.project.ModifyProject;
 import com.asdf.JavaProject.service.ProjectService;
@@ -29,94 +27,94 @@ public class ProjectController {
     private void addPerson(@RequestHeader Map<String, String> header, @PathVariable("project_id") String projectId){
         projectService.addPersonnel(header.get("authorization").substring(7), projectId);
     }
-
-    @PutMapping("modify/{project_id}")
-    private ResponseEntity<String> modifyProject(@RequestHeader Map<String, String> header, @PathVariable("project_id") String projectId, @RequestBody ModifyProject modifyProject){
-        try{
-            projectService.modifyProject(header.get("authorization").substring(7), projectId, modifyProject);
-            return new ResponseEntity<>("modified", HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @DeleteMapping("delete/{project_id}")
-    private ResponseEntity<String> deleteProject(@RequestHeader Map<String, String> header, @PathVariable("project_id") String projectId){
-        try{
-            projectService.deleteProject(header.get("authorization").substring(7), projectId);
-            return new ResponseEntity<>("deleted", HttpStatus.OK);
-        }
-        catch(Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    //일정 추가
-    @PostMapping("schedule/{project_id}")
-    private ResponseEntity<String> addSchedule(@RequestHeader Map<String, String> header, @PathVariable("project_id") String projectId, @RequestBody CreateSchedule createSchedule){
-        try{
-            return new ResponseEntity<>(projectService.addSchedule(header.get("authorization").substring(7), projectId, createSchedule), HttpStatus.CREATED);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    //일정 수정
-    @PutMapping("schedule/{schedule_id}")
-    private ResponseEntity<String> modifySchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId, @RequestBody ModifySchedule modifySchedule){
-        try{
-            return new ResponseEntity<>(projectService.modifySchedule(header.get("authorization").substring(7), scheduleId, modifySchedule), HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @DeleteMapping("deleteSchedule/{schedule_id}")
-    private ResponseEntity<String> deleteSchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId){
-        try{
-            return new ResponseEntity<>(projectService.deleteSchedule(header.get("authorization").substring(7), scheduleId), HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @GetMapping("complete/{schedule_id}")
-    private ResponseEntity<String> addSchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId){
-        try{
-            return new ResponseEntity<>(projectService.completeSchedule(header.get("authorization").substring(7), scheduleId), HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @GetMapping("join/{schedule_id}")
-    private ResponseEntity<String> joinSchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId){
-        try{
-            projectService.joinSchedule(header.get("authorization").substring(7), scheduleId);
-            return new ResponseEntity<>("Participated", HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @DeleteMapping("leave/{schedule_id}")
-    private ResponseEntity<String> leaveSchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId){
-        try{
-            projectService.leaveSchedule(header.get("authorization").substring(7), scheduleId);
-            return new ResponseEntity<>("left", HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @PatchMapping("changeRole/{role}")
-    private ResponseEntity<String> changeRole(@RequestHeader Map<String, String> header, @PathVariable("role") String role){
-        try{
-            projectService.changingRoles(header.get("authorization").substring(7), role);
-            return new ResponseEntity<>("changed", HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
+//
+//    @PutMapping("modify/{project_id}")
+//    private ResponseEntity<String> modifyProject(@RequestHeader Map<String, String> header, @PathVariable("project_id") String projectId, @RequestBody ModifyProject modifyProject){
+//        try{
+//            projectService.modifyProject(header.get("authorization").substring(7), projectId, modifyProject);
+//            return new ResponseEntity<>("modified", HttpStatus.OK);
+//        }
+//        catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    @DeleteMapping("delete/{project_id}")
+//    private ResponseEntity<String> deleteProject(@RequestHeader Map<String, String> header, @PathVariable("project_id") String projectId){
+//        try{
+//            projectService.deleteProject(header.get("authorization").substring(7), projectId);
+//            return new ResponseEntity<>("deleted", HttpStatus.OK);
+//        }
+//        catch(Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    //일정 추가
+//    @PostMapping("schedule/{project_id}")
+//    private ResponseEntity<String> addSchedule(@RequestHeader Map<String, String> header, @PathVariable("project_id") String projectId, @RequestBody CreateSchedule createSchedule){
+//        try{
+//            return new ResponseEntity<>(projectService.addSchedule(header.get("authorization").substring(7), projectId, createSchedule), HttpStatus.CREATED);
+//        }catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    //일정 수정
+//    @PutMapping("schedule/{schedule_id}")
+//    private ResponseEntity<String> modifySchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId, @RequestBody ModifySchedule modifySchedule){
+//        try{
+//            return new ResponseEntity<>(projectService.modifySchedule(header.get("authorization").substring(7), scheduleId, modifySchedule), HttpStatus.OK);
+//        }catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    @DeleteMapping("deleteSchedule/{schedule_id}")
+//    private ResponseEntity<String> deleteSchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId){
+//        try{
+//            return new ResponseEntity<>(projectService.deleteSchedule(header.get("authorization").substring(7), scheduleId), HttpStatus.OK);
+//        }catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    @GetMapping("complete/{schedule_id}")
+//    private ResponseEntity<String> addSchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId){
+//        try{
+//            return new ResponseEntity<>(projectService.completeSchedule(header.get("authorization").substring(7), scheduleId), HttpStatus.OK);
+//        }catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    @GetMapping("join/{schedule_id}")
+//    private ResponseEntity<String> joinSchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId){
+//        try{
+//            projectService.joinSchedule(header.get("authorization").substring(7), scheduleId);
+//            return new ResponseEntity<>("Participated", HttpStatus.OK);
+//        }catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    @DeleteMapping("leave/{schedule_id}")
+//    private ResponseEntity<String> leaveSchedule(@RequestHeader Map<String, String> header, @PathVariable("schedule_id") String scheduleId){
+//        try{
+//            projectService.leaveSchedule(header.get("authorization").substring(7), scheduleId);
+//            return new ResponseEntity<>("left", HttpStatus.OK);
+//        }catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    @PatchMapping("changeRole/{role}")
+//    private ResponseEntity<String> changeRole(@RequestHeader Map<String, String> header, @PathVariable("role") String role){
+//        try{
+//            projectService.changingRoles(header.get("authorization").substring(7), role);
+//            return new ResponseEntity<>("changed", HttpStatus.OK);
+//        }catch (Exception e){
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
 }
